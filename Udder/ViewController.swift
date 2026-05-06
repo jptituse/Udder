bb0aa81f956235c1813d73dcf452af12e6bdd3ed
Jonathan Titus <jptitus2@gmail.com>
2026-05-05 19:38:51 -0700

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Jonathan Titus on 5/5/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
